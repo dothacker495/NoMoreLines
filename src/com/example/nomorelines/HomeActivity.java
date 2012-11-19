331abca821e94c2a25e3f9f10fefe568d89ae7d7
Raphael Pineda <dakmatz495@yahoo.com>
2012-11-19 23:08:54 +0800

    Integrated the HomeActivity with a Collapsible Search bar and added androidsherlock library with its layouts, menus and values

--- src/com/example/nomorelines/HomeActivity.java
+++ src/com/example/nomorelines/HomeActivity.java
@@ -1,20 +1,33 @@
 package com.example.nomorelines;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class HomeActivity extends Activity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+
+public class HomeActivity extends SherlockActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(SampleList.THEME); //Used for theme switching in samples
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.text);
+        ((TextView)findViewById(R.id.text)).setText("");
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_home, menu);
+        //Used to put dark icons on light action bar
+        boolean isLight = SampleList.THEME == R.style.Theme_Sherlock_Light;
+
+        menu.add("Search")
+            .setIcon(isLight ? R.drawable.ic_search_inverse : R.drawable.ic_search)
+            .setActionView(R.layout.collapsible_edittext)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+
         return true;
     }
 }
